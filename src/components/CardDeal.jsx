777b96aeed9bb115a654eb83b55e2de7c2663be0
Luispaguti <l.palaciosgutierrez@gmail.com>
2023-01-07 18:58:23 +0100

    testimonials

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { card } from '../assets/index'
+import styles, { layout } from '../style';
+import Button from './Button';
 
-function CardDeal() {
-  return (
-    <div>CardDeal</div>
+const CardDeal = () => (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, quisquam. Accusantium, natus itaque ab asperiores quos sint praesentium inventore nisi ipsa reiciendis dolores omnis, doloremque quaerat incidunt quibusdam! A, similique.</p>
+        
+        <Button styles="mt-10"/>
+      </div>
+
+      
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]'/>
+      </div>
+    </section>
   )
-}
+
 
 export default CardDeal
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { feedback } from '../constant/index';
+import styles from '../style';
+import FeedbackCard from './FeedbackCard';
 
-function Testimonials() {
-  return (
-    <div>Testimonials</div>
+const Testimonials = () => (
+    <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+      
+      <div/>
+
+      <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+        <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h1>
+      </div>
+    </section>
   )
-}
+
 
 export default Testimonials
